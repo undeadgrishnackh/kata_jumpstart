1cf84392f71b2bef870d8c31169f8070d604038d
Michele Brissoni <michele.brissoni@ibm.com@Micheles-MacBook-Pro-2.local>
2021-01-29 16:47:03 +0100

    fix: the iteration test looping time, a typo

--- test/integration_test.codacy.sh
+++ test/integration_test.codacy.sh
@@ -3,9 +3,9 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${DIR}/../src/functions.inspec.sh
 
 
-for i in {1..6}; do     
+for i in {1..5}; do     
   inspec exec ${DIR}/_codacy.spec.rb --interactive --color --enable-telemetry --show-progress
   [[ $? -eq 0 ]] && break || echo "\n⏱️  it can take a while to create the project in Codacy...";
-  echo "⏱️ next try in 10 seconds"    
-  sleep 10
+  echo "⏱️  next try in 60 seconds"    
+  sleep 60
 done
--- test/integration_test.sonarcloud.sh
+++ test/integration_test.sonarcloud.sh
@@ -3,10 +3,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 source ${DIR}/../src/functions.inspec.sh
 
 
-for i in {1..6}; do     
+for i in {1..5}; do     
   inspec exec ${DIR}/_sonarcloud.spec.rb --interactive --color --enable-telemetry --show-progress
   [[ $? -eq 0 ]] && break || echo "⏱️  it can take a while to create the project in SonarCloud...";    
-  echo "⏱️  next try in 10 seconds"    
-  sleep 10
+  echo "⏱️  next try in 60 seconds"    
+  sleep 60
 done
 
